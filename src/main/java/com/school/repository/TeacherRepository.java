cb5cfff760233075d1c8ef3be8ce776e0ee8c422
Vlad <bulakovskijvladislav@gmail.com>
2024-05-06 21:26:08 +0300

    Created TeacherRepository

---
+++ src/main/java/com/school/repository/TeacherRepository.java
@@ -0,0 +1,8 @@
+package com.school.repository;
+
+import com.school.models.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+
+}
