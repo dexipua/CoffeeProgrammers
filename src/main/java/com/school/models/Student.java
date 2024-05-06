6145b1d028b1d6155c7e63c396c90945929111aa
Vadym Honcharuk <vadymhoncharuk06@gmail.com>
2024-05-06 16:30:59 +0300

    made Student model

--- src/main/java/com/school/models/Student.java
+++ src/main/java/com/school/models/Student.java
@@ -1,4 +1,52 @@
 package com.school.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@Setter
+@Getter
+@Table(name = "students")
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToMany
+    private List<Object> subjects; // TODO
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    private User user;
+
+    @Override
+    public String toString(){
+        return "Student{" +
+                "id=" + id +
+                ", subjects=" + subjects +
+                ", user=" + user +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if(this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Student student = (Student)o;
+        return this.id == student.id && this.subjects.equals(student.subjects) && user.equals(student.user);
+    }
+
+    @Override
+    public int hashCode(){
+        int result = Long.hashCode(id);
+        result = 17 * result + subjects.hashCode();
+        result = 17 * result + user.hashCode();
+        return result;
+    }
 }
