5a178ef05a586445220adf1877ab0aeefcbdb81b
Artem <ma.ua@outlook.com>
2024-05-07 20:45:08 +0300

    Add TeacherRepository

--- src/main/java/com/school/repository/TeacherRepository.java
+++ src/main/java/com/school/repository/TeacherRepository.java
@@ -7,9 +7,11 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
 
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+    Optional<Teacher> findById(long id);
+    @Query("select t from Teacher t where t.user.id =: id")
+    Optional<Teacher> findByUserId(long id);
     @Query("select t from Teacher t where t.user.email =: email")
     Optional<Teacher> findByEmail(String email);
-
     @Query("select t from Teacher t where t.user.username =: username")
     Optional<Teacher> findByUsername(String username);
 }
