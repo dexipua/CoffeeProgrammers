faa98188dd3c7cba843741b1984da369e5cb6108
Vadym Honcharuk <vadymhoncharuk06@gmail.com>
2024-05-07 20:16:53 +0300

    added some code in StudentRepository

--- src/main/java/com/school/repository/StudentRepository.java
+++ src/main/java/com/school/repository/StudentRepository.java
@@ -1,11 +1,16 @@
 package com.school.repository;
 
 import com.school.models.Student;
+import com.school.models.Subject;
 import com.school.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface StudentRepository extends JpaRepository<Student, Long> {
-    Student findByStudentId(Long studentId);
-    Student findByEmail(String email);
-    Student findByUsername(String username);
+    Optional<Student> findByStudentId(Long studentId);
+    Optional<List<Subject>> findBySubjects(Long studentId);
+    Optional<Student> findByEmail(String email);
+    Optional<Student> findByUsername(String username);
 }
