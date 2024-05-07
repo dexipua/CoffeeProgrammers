759bae48c49b103c5dc97932fa3366320ae6ca79
Artem <ma.ua@outlook.com>
2024-05-07 20:42:22 +0300

    Add optional

--- src/main/java/com/school/repository/SubjectRepository.java
+++ src/main/java/com/school/repository/SubjectRepository.java
@@ -1,14 +1,16 @@
 package com.school.repository;
 
+import com.school.models.Role;
 import com.school.models.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
+    Optional<Subject> findById(Long id);
     Optional<Subject> findByName(String name);
     Optional<List<Subject>> findAllByOrderByName();
     Optional<List<Subject>> findByTeacher_Id(long teacherId);
-
 }
--- src/main/java/com/school/repository/UserRepository.java
+++ src/main/java/com/school/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.school.repository;
 
+import com.school.models.Role;
 import com.school.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findById(Long id);
     Optional<User> findByEmail(String email);
     Optional<User> findByUsername(String username);
 }
