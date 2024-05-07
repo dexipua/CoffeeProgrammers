12dde7ed4d0c74baec740728c8cf630122df977e
Artem <ma.ua@outlook.com>
2024-05-07 19:54:47 +0300

    Add optional

--- src/main/java/com/school/repository/SubjectRepository.java
+++ src/main/java/com/school/repository/SubjectRepository.java
@@ -4,10 +4,11 @@ import com.school.models.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
-    Subject findByName(String name);
-    List<Subject> findAllByOrderByName();
-    List<Subject> findByTeacher_Id(long teacherId);
+    Optional<Subject> findByName(String name);
+    Optional<List<Subject>> findAllByOrderByName();
+    Optional<List<Subject>> findByTeacher_Id(long teacherId);
 
 }
