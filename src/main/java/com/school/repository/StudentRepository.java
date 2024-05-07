3c1ba26d6f72ac6982e7cf17151c826aa43e9c41
Artem <ma.ua@outlook.com>
2024-05-07 21:15:29 +0300

    Merge all in one complete

--- src/main/java/com/school/repository/StudentRepository.java
+++ src/main/java/com/school/repository/StudentRepository.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 public interface StudentRepository extends JpaRepository<Student, Long> {
     Optional<Student> findByStudentId(Long studentId);
     @Query("SELECT s FROM Student s JOIN s.subjects sub WHERE sub.name = :subjectName")
-    List<Student> findBySubjectName(String subjectName);
+    Optional<List<Student>> findBySubjectName(String subjectName);
     Optional<Student> findByEmail(String email);
     Optional<Student> findByUsername(String username);
 }
--- src/main/java/com/school/repository/SubjectRepository.java
+++ src/main/java/com/school/repository/SubjectRepository.java
@@ -1,6 +1,7 @@
 package com.school.repository;
 
 import com.school.models.Role;
+import com.school.models.Student;
 import com.school.models.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,4 +14,6 @@ public interface SubjectRepository extends JpaRepository<Subject, Long> {
     Optional<Subject> findByName(String name);
     Optional<List<Subject>> findAllByOrderByName();
     Optional<List<Subject>> findByTeacher_Id(long teacherId);
+    @Query("SELECT s FROM Subject s JOIN s.students sub WHERE sub.user.username = :studentName")
+    List<Student> findByStudentUsername(String studentName);
 }
