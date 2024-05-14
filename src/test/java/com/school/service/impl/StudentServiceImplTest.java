4e501a88f6689e32e775ea22c1af915756e2baf0
Vadym Honcharuk <vadymhoncharuk06@gmail.com>
2024-05-14 21:15:19 +0300

    do some changes in class StudentServiceImplTest again

--- src/test/java/com/school/service/impl/StudentServiceImplTest.java
+++ src/test/java/com/school/service/impl/StudentServiceImplTest.java
@@ -13,7 +13,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
