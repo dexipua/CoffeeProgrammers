5e3ba6de079d1e23c2ebf29e1b9c98adafe5841e
Vadym Honcharuk <vadymhoncharuk06@gmail.com>
2024-05-18 23:22:04 +0300

    create StudentResponse

---
+++ src/main/java/com/school/dto/StudentResponse.java
@@ -0,0 +1,31 @@
+package com.school.dto;
+
+import com.school.models.Student;
+import com.school.models.Subject;
+import lombok.*;
+
+
+import java.util.List;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class StudentResponse {
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private long id;
+    private List<Subject> subjects;
+
+    public StudentResponse(Student student){
+        this.username = student.getUser().getUsername();
+        this.firstName = student.getUser().getFirstName();
+        this.lastName = student.getUser().getLastName();
+        this.email = student.getUser().getEmail();
+        this.id = student.getId();
+        this.subjects = student.getSubjects();
+    }
+}
