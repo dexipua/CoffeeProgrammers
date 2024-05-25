6aad81c87ef73834d6e4f04f5ba799516b487b7c
Artem <ma.ua@outlook.com>
2024-05-25 21:53:27 +0300

    Delete username in user

--- src/main/resources/db/migrations/V00009__insert_information_in_users_table.sql
+++ src/main/resources/db/migrations/V00009__insert_information_in_users_table.sql
@@ -1,14 +1,14 @@
-insert into users (username, first_name, last_name, email, password, role_id)
-values ('Dexip', 'Artem', 'Moseichenko', 'am@gmail.com', '$2a$12$tCc7xjpcZZg0PT57MxYmX.3Pp2gedmXUHfS86IHz5Y8RUoIKq.Ma6', '1');
-insert into users (username, first_name, last_name, email, password, role_id)
-values ('Boh', 'Bohdan', 'Lutvyn', 'bl@gmail.com', '$2a$12$tCc7xjpcZZg0PT57MxYmX.3Pp2gedmXUHfS86IHz5Y8RUoIKq.Ma6', '2');
-insert into users (username, first_name, last_name, email, password, role_id)
-values ('Son', 'Sonya', 'Burma', 'sb@gmail.com', '$2a$12$tCc7xjpcZZg0PT57MxYmX.3Pp2gedmXUHfS86IHz5Y8RUoIKq.Ma6', '2');
-insert into users (username, first_name, last_name, email, password, role_id)
-values ('Vik', 'Viktor', 'Linkoln', 'vl@gmail.com', '$2a$12$tCc7xjpcZZg0PT57MxYmX.3Pp2gedmXUHfS86IHz5Y8RUoIKq.Ma6', '2');
-insert into users (username, first_name, last_name, email, password, role_id)
-values ('Vic', 'Victoria', 'Kornienko', 'vk@gmail.com', '$2a$12$tCc7xjpcZZg0PT57MxYmX.3Pp2gedmXUHfS86IHz5Y8RUoIKq.Ma6', '3');
-insert into users (username, first_name, last_name, email, password, role_id)
-values ('Gle', 'Gleb', 'Popov', 'gp@gmail.com', '$2a$12$tCc7xjpcZZg0PT57MxYmX.3Pp2gedmXUHfS86IHz5Y8RUoIKq.Ma6', '3');
-insert into users (username, first_name, last_name, email, password, role_id)
-values ('Vla', 'Victor', 'Popov', 'vs@gmail.com', '$2a$12$tCc7xjpcZZg0PT57MxYmX.3Pp2gedmXUHfS86IHz5Y8RUoIKq.Ma6', '3');
+insert into users (first_name, last_name, email, password, role_id)
+values ('Artem', 'Moseichenko', 'am@gmail.com', '$2a$12$tCc7xjpcZZg0PT57MxYmX.3Pp2gedmXUHfS86IHz5Y8RUoIKq.Ma6', '1');
+insert into users (first_name, last_name, email, password, role_id)
+values ('Bohdan', 'Lutvyn', 'bl@gmail.com', '$2a$12$tCc7xjpcZZg0PT57MxYmX.3Pp2gedmXUHfS86IHz5Y8RUoIKq.Ma6', '2');
+insert into users (first_name, last_name, email, password, role_id)
+values ('Sonya', 'Burma', 'sb@gmail.com', '$2a$12$tCc7xjpcZZg0PT57MxYmX.3Pp2gedmXUHfS86IHz5Y8RUoIKq.Ma6', '2');
+insert into users (first_name, last_name, email, password, role_id)
+values ('Viktor', 'Linkoln', 'vl@gmail.com', '$2a$12$tCc7xjpcZZg0PT57MxYmX.3Pp2gedmXUHfS86IHz5Y8RUoIKq.Ma6', '2');
+insert into users (first_name, last_name, email, password, role_id)
+values ('Victoria', 'Kornienko', 'vk@gmail.com', '$2a$12$tCc7xjpcZZg0PT57MxYmX.3Pp2gedmXUHfS86IHz5Y8RUoIKq.Ma6', '3');
+insert into users (first_name, last_name, email, password, role_id)
+values ('Gleb', 'Popov', 'gp@gmail.com', '$2a$12$tCc7xjpcZZg0PT57MxYmX.3Pp2gedmXUHfS86IHz5Y8RUoIKq.Ma6', '3');
+insert into users (first_name, last_name, email, password, role_id)
+values ('Victor', 'Popov', 'vs@gmail.com', '$2a$12$tCc7xjpcZZg0PT57MxYmX.3Pp2gedmXUHfS86IHz5Y8RUoIKq.Ma6', '3');
