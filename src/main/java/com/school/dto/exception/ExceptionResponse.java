cf355afa7e930b6b00e7683802cce2a35793a48a
Yaroslav Holobyn <slavaholobyn@gmail.com>
2024-05-30 23:38:30 +0300

    Some change in ExceptionResponse.java

--- src/main/java/com/school/dto/exception/ExceptionResponse.java
+++ src/main/java/com/school/dto/exception/ExceptionResponse.java
@@ -1,13 +1,15 @@
 package com.school.dto.exception;
 
+import lombok.Data;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 import java.time.ZonedDateTime;
 
 @Getter
+@Data
 public class ExceptionResponse {
-    private final String message;
+    private String message;
 
     public ExceptionResponse(String message) {
         this.message = message;
