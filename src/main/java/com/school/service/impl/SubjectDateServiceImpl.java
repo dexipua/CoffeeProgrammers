fcafd8e7862a8d1a47266e97fe22906b8dd2f1f0
Artem <ma.ua@outlook.com>
2024-06-02 18:19:40 +0300

    Add methods in controller

--- src/main/java/com/school/service/impl/SubjectDateServiceImpl.java
+++ src/main/java/com/school/service/impl/SubjectDateServiceImpl.java
@@ -4,12 +4,14 @@ import com.school.models.Subject;
 import com.school.models.SubjectDate;
 import com.school.repositories.SubjectDateRepository;
 import com.school.service.SubjectDateService;
+import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.DayOfWeek;
 import java.util.HashMap;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -19,11 +21,20 @@ public class SubjectDateServiceImpl implements SubjectDateService {
 
     @Override
     public SubjectDate create(SubjectDate subjectDate) {
+        if(subjectDateRepository.findAll().stream().filter(subjectDate1 ->
+                subjectDate1.getDayOfWeek().equals(subjectDate.getDayOfWeek())
+                && subjectDate1.getNumOfLesson().equals(subjectDate.getNumOfLesson())).count() == 1){
+            throw new EntityExistsException("Subject on this time and date already exists");
+        }
         return subjectDateRepository.save(subjectDate);
     }
 
     @Override
     public SubjectDate update(SubjectDate subjectDate) {
+        SubjectDate find = subjectDateRepository.findAll().stream().filter(subjectDate1
+                -> subjectDate1.getDayOfWeek().equals(subjectDate.getDayOfWeek())
+                        && subjectDate1.getNumOfLesson().equals(subjectDate.getNumOfLesson())).toList().get(1);
+
         return subjectDateRepository.save(subjectDate);
     }
 
