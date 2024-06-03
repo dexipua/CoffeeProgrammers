1e91a268872ad8873bed9f8f91a10ed5dc31ba23
Artem <ma.ua@outlook.com>
2024-06-03 17:25:41 +0300

    Optimized create and update in subjectDate

--- src/main/java/com/school/repositories/SubjectDateRepository.java
+++ src/main/java/com/school/repositories/SubjectDateRepository.java
@@ -1,11 +1,18 @@
 package com.school.repositories;
 
+import com.school.models.Student;
 import com.school.models.SubjectDate;
+import com.school.models.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
+import java.time.DayOfWeek;
 import java.util.List;
 
 public interface SubjectDateRepository extends JpaRepository<SubjectDate, Long> {
     List<SubjectDate> findAllBySubject_Students_Id(@Param("studentId") long studentId);
+    List<SubjectDate> findAllByDayOfWeekAndNumOfLessonAndSubject_Teacher(DayOfWeek dayOfWeek, SubjectDate.NumOfLesson numOfLesson, Teacher teacher);
+    List<SubjectDate> findAllByDayOfWeekAndNumOfLessonAndSubject_StudentsContains(DayOfWeek dayOfWeek, SubjectDate.NumOfLesson numOfLesson, Student student);
+    List<SubjectDate> findAllByDayOfWeekAndNumOfLessonAndSubject_TeacherAndIdIsNot(DayOfWeek dayOfWeek, SubjectDate.NumOfLesson numOfLesson, Teacher teacher, long id);
+    List<SubjectDate> findAllByDayOfWeekAndNumOfLessonAndSubject_StudentsContainsAndIdIsNot(DayOfWeek dayOfWeek, SubjectDate.NumOfLesson numOfLesson, Student student, long id);
 }
