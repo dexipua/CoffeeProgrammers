7ff2b5f6ae7d7a564dd0acdb0523436c6465e702
Artem <ma.ua@outlook.com>
2024-06-06 19:59:33 +0300

    Change method in AuthEntryPointJwt

--- src/main/java/com/school/config/JWT/AuthEntryPointJwt.java
+++ src/main/java/com/school/config/JWT/AuthEntryPointJwt.java
@@ -10,13 +10,26 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import java.util.List;
+
 @Component
 @Slf4j
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
 
+    private static final List<Class<? extends AuthenticationException>> IGNORED_EXCEPTIONS = List.of(
+
+    );
+
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        log.error("AuthenticationException: {}", authException.getMessage());
+        if (shouldLogException(authException)) {
+            log.error("AuthenticationException: {}", authException.getMessage());
+        }
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
     }
+
+    private boolean shouldLogException(AuthenticationException exception) {
+        return IGNORED_EXCEPTIONS.stream().noneMatch(ignoredException -> ignoredException.isInstance(exception));
+    }
 }
+
