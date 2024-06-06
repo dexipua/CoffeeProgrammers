eb7a2698e0924346a97d14bb56ee46376fcf8ef6
Artem <ma.ua@outlook.com>
2024-06-06 19:41:34 +0300

    Add working get all students and teachers

--- src/main/java/com/school/repositories/StudentRepository.java
+++ src/main/java/com/school/repositories/StudentRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 public interface StudentRepository extends JpaRepository<Student, Long> {
     @Query("SELECT s.students FROM Subject s WHERE LOWER(s.name) LIKE LOWER(CONCAT('%', :subjectName, '%'))")
     List<Student> findStudentBySubjectNameContainingIgnoreCase(String subjectName);
-    List<Student> findAllByOrderByUser();
+    List<Student> findAllByOrderByUser_LastNameAsc();
     List<Student> findAllByUser_FirstNameContainingIgnoreCaseAndUser_LastNameContainingIgnoreCase(String firstName, String lastName);
     @Query("SELECT s.students FROM Subject s WHERE s.teacher.id = :teacherId")
     List<Student> findAllByTeacherId(long teacherId);
